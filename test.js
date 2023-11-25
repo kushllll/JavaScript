52db828c599026cef1b8689ef94e27e8e95aef8e
Kushal Bhalwal <101339957+kushllll@users.noreply.github.com>
2023-11-25 21:15:50 +0000

    Container config done

---
+++ test.js
@@ -0,0 +1,20 @@
+// console.log("kushal");
+
+const accountId = 1432324
+let accountEmail = "keshal@gmail.com"
+var accountPassword = "12312324";
+
+    // accountId = 2 // not allowed
+
+
+    accountEmail= "kushal@gmail.com"
+    accountPassword ="54654"
+    console.log(accountEmail); //it take so much time id we do this code to print
+                               // not giving so much time in this
+                               // i use different code for it
+                               // named as console.table([]);
+    console.table([accountEmail, accountPassword]); //so when i execute this it show me the table of outputs which easy to look and save time:)
+    /*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
