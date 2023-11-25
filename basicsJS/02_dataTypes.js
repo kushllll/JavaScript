c1760a2ee134e3d7c2856bd756df6e56ec24c7d0
Kushal Bhalwal <101339957+kushllll@users.noreply.github.com>
2023-11-25 22:06:16 +0000

    datatypes

---
+++ basicsJS/02_dataTypes.js
@@ -0,0 +1,32 @@
+        "usestrict"; //treat all js code as newear version
+
+        //alert(9+9) //if we execute this code, it shows us a error. because we using node.js right now 
+                    // not browser consol.
+
+
+
+        console.log(3 
+            +
+            3) // code readability should be high
+
+        console.log("Hitesh")
+
+
+        let name = "hitesh"
+        let age = 18
+        let isLoggedIn = false
+        let state;
+
+        // number => 2 to power 53
+        // bigint
+        // string => ""
+        // boolean => true/false
+        // null => standalone value
+        // undefined => 
+        // symbol => unique
+
+
+        // object
+
+        console.log(typeof undefined); // undefined
+        console.log(typeof null); // object
